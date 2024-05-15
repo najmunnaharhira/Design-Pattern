e31a18e1cb0fc9d53c508160484c1a0544e088fc
Najmun Nahar Hira <107489391+najmunnaharhira@users.noreply.github.com>
2024-05-15 22:43:39 +0600

    Update ChessePizza.java

---
+++ AbstractDesign/ChessePizza.java
@@ -0,0 +1,11 @@
+
+// Concrete pizza class
+public class CheesePizza extends Pizza {
+    public CheesePizza() {
+        description = "Cheese Pizza";
+    }
+
+    public double cost() {
+        return 8.99;
+    }
+}
